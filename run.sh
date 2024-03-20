75f5fd88e377fac32d215a7db5deae6590611770
yiannisha <hadjiyiannis.3@gmail.com>
2024-03-20 23:53:02 +0200

    Set output to stderr

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ executable="vdc-main"
 
 # Check if the program is running
 if pgrep "$executable" >/dev/null; then
-    echo "Program 'vdc-main' already running at `pidof vdc-main`."
+    echo "Program 'vdc-main' already running at `pidof vdc-main`." >&2
 else
     sudo ~/vdc-controller/dist/vdc-main
 fi
